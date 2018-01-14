4fbecd4c0f085f8ecece90d3a631dabd40027eac
Clancy23 <35420842+Clancy23@users.noreply.github.com>
2018-01-14 19:30:03 +0800

    登录界面设计

---
+++ ScoreManagementSystem1.0/ScoreManagementSystem1.0/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ScoreManagementSystem1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            #region 设置为单选
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (i != e.Index)
+                {
+                    checkedListBox1.SetItemCheckState(i, System.Windows.Forms.CheckState.Unchecked);
+                }
+            }
+            #endregion
+        }
+    }
+}
